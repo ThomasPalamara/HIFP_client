5e77606ec8208715d1e007f0393b2e8d940e9997
ThomasPalamara <palamara.thomas@gmail.com>
2021-12-17 12:56:16 +0000

    version 1

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import Vocabulary from './src/pages/Vocabulary';
+import { DocProvider } from './src/context/doc';
+import { Box, NativeBaseProvider, Text, View } from 'native-base';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <DocProvider>
+      <NativeBaseProvider>
+        <View bgColor="lightBlue.50" p={10} pt={24} style={{ flex: 1 }}>
+          <Box
+            style={styles.background}
+            bgColor="blue.700"
+            minW="full"
+            minH="1/2"
+          ></Box>
+          <View mb="24">
+            <Text fontSize="4xl" color="white" fontWeight="bold">
+              Vocabulary Test
+            </Text>
+            <Text pl={4} fontSize="md" color="white" fontWeight="bold">
+              You can do it &#10084;
+            </Text>
+          </View>
+          <View style={styles.container}>
+            <Vocabulary></Vocabulary>
+          </View>
+        </View>
+      </NativeBaseProvider>
+    </DocProvider>
   );
-}
+};
+export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    position: 'relative',
+  },
+  background: {
+    borderRadius: 20,
+    position: 'absolute',
+    top: '-50%',
+    left: 0,
+    right: 0,
+    bottom: '70%',
   },
 });
---
+++ src/components/Card.jsx
@@ -0,0 +1,115 @@
+import { Box, Button, Center, HStack, Text, VStack } from 'native-base';
+import React, { useState } from 'react';
+import { capitalize } from 'lodash';
+
+const Card = ({ word, newWord }) => {
+  const [reveal, setReveal] = useState(false);
+  const [hint, setHint] = useState(-1);
+  if (!word) return null;
+  const outOfHints = hint >= word.french.length || 0;
+  const onReveal = () => {
+    if (!reveal) {
+      setReveal(true);
+    } else {
+      setReveal(false);
+      setHint(-1);
+      newWord();
+    }
+  };
+  const onHint = () => {
+    if (!outOfHints) setHint(hint + 1);
+  };
+
+  const answerText = () => {
+    if (reveal) {
+      return capitalize(word.french);
+    } else if (hint > -1) {
+      return capitalize(
+        [...word.french]
+          .map((char, i) => {
+            if (i < hint) {
+              return char;
+            } else {
+              switch (char) {
+                case ' ':
+                  return ' .';
+                case '/':
+                  return ' /';
+                case "'":
+                  return char;
+                default:
+                  return ' _';
+              }
+            }
+          })
+          .join('')
+      );
+    }
+    return '?';
+  };
+  return (
+    <VStack>
+      <Box
+        minWidth="full"
+        rounded="lg"
+        py="12"
+        px="3"
+        mb="10"
+        shadow="1"
+        _dark={{
+          borderColor: 'coolGray.600',
+          backgroundColor: 'gray.700',
+        }}
+        _web={{
+          shadow: 2,
+          borderWidth: 0,
+        }}
+        _light={{
+          backgroundColor: 'white',
+        }}
+      >
+        <Center>
+          <Box minH={20} display="flex" justifyContent="center" mb={7}>
+            <Text fontSize="3xl" fontWeight="bold">
+              {capitalize(word.english)}
+            </Text>
+          </Box>
+
+          <Box minH={20} display="flex" justifyContent="center">
+            <Text fontSize="3xl">{answerText()}</Text>
+          </Box>
+        </Center>
+      </Box>
+
+      <Box>
+        <HStack justifyContent="space-around">
+          <Button
+            onPress={onHint}
+            minW="24"
+            py="4"
+            rounded="lg"
+            disabled={outOfHints}
+            colorScheme="darkBlue"
+          >
+            <Text fontSize="lg" fontWeight="bold" color="white">
+              Hint
+            </Text>
+          </Button>
+
+          <Button
+            onPress={onReveal}
+            minW="24"
+            rounded="lg"
+            colorScheme={!reveal ? 'amber' : 'success'}
+          >
+            <Text fontSize="lg" fontWeight="bold" color="white">
+              {reveal ? 'Next' : 'Answer'}
+            </Text>
+          </Button>
+        </HStack>
+      </Box>
+    </VStack>
+  );
+};
+
+export default Card;
